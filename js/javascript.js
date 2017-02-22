dd6df1cc5ff05dc03ad95ac7e8149641aedec9ba
Krzysztof Gagacki <krzysztofgagacki@gmail.com>
2017-02-22 03:11:08 +0100

    final JS

--- js/javascript.js
+++ js/javascript.js
@@ -18,30 +18,21 @@ function fadeCaption() {
 	}, 1000);
 }
 
-function slideright() {
-	if (slideIndex > 0) {
+function slide(n) {
+	if ((slideIndex > 0 && n == -1) || (slideIndex < slideCount - 1 && n == 1)) {
 		$("p.slider-caption").css({
 			opacity: 0
 		});
-		slideIndex -= 1;
+		slideIndex += n;
 		var singleSlideLeft = ((-100 * slideIndex) + "vw");
 		$("#slide-show").animate({
 			left: singleSlideLeft
 		}, 1000, "linear", fadeCaption)
 	}
 }
-
-function slideleft() {
-	if (slideIndex < slideCount - 1) {
-		$("p.slider-caption").css({
-			opacity: 0
-		});
-		slideIndex += 1;
-		var singleSlideLeft = ((-100 * slideIndex) + "vw");
-		$("#slide-show").animate({
-			left: singleSlideLeft
-		}, 1000, "linear", fadeCaption);
-	}
-}
-$("a.prev-slide").on("click", slideright);
-$("a.next-slide").on("click", slideleft);
+$("a.prev-slide").on("click", function () {
+	slide(-1)
+});
+$("a.next-slide").on("click", function () {
+	slide(1)
+});
